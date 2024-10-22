18940cb7be386a3f1bc47e5a63715a7e5fa54843
keaimk <fly1043@naver.com>
2024-10-22 09:26:36 +0900

    최초 커밋

---
+++ src/main/java/com/example/prjjsp/PrjJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrjJspApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/prjjsp/PrjJspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prjjsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjJspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
