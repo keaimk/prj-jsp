d27e5e8331088fbfc00728efcb1282eaa315781e
keaimk <fly1043@naver.com>
2024-10-25 09:44:50 +0900

    회원 정보 수정

--- src/main/java/com/example/prjjsp/service/MemberService.java
+++ src/main/java/com/example/prjjsp/service/MemberService.java
@@ -30,4 +30,8 @@ public class MemberService {
         int cnt = mapper.deleteByIdAndPassword(id, password);
         return cnt == 1;
     }
+
+    public void update(Member member) {
+        mapper.update(member);
+    }
 }
