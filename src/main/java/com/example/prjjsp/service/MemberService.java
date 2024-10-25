8b14f7e01baf84817d7fd2a1464d2320928716e3
keaimk <fly1043@naver.com>
2024-10-25 10:53:32 +0900

    로그인 화면

--- src/main/java/com/example/prjjsp/service/MemberService.java
+++ src/main/java/com/example/prjjsp/service/MemberService.java
@@ -39,4 +39,8 @@ public class MemberService {
         int cnt = mapper.updatePassword(id, oldPassword, newPassword);
         return cnt == 1;
     }
+
+    public Member get(String id, String password) {
+        return mapper.selectByIdPassword(id, password);
+    }
 }
