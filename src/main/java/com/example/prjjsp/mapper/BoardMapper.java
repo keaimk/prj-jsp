860d9502440aac269078d0ddbdf288bce360744e
keaimk <fly1043@naver.com>
2024-10-25 11:55:04 +0900

    글 작성 시 로그인한 사용자의 아이디 입력

--- src/main/java/com/example/prjjsp/mapper/BoardMapper.java
+++ src/main/java/com/example/prjjsp/mapper/BoardMapper.java
@@ -1,6 +1,7 @@
 package com.example.prjjsp.mapper;
 
 import com.example.prjjsp.dto.Board;
+import com.example.prjjsp.dto.Member;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -12,10 +13,10 @@ public interface BoardMapper {
     @Insert("""
             INSERT INTO board
             (title, content, writer)
-            VALUES (#{title}, #{content}, #{writer})
+            VALUES (#{board.title}, #{board.content}, #{member.id})
             """)
-    @Options(useGeneratedKeys = true, keyProperty = "id")
-    int insert(Board board);
+    @Options(useGeneratedKeys = true, keyProperty = "board.id")
+    int insert(Board board, Member member);
 
     @Select("""
             SELECT * FROM board
